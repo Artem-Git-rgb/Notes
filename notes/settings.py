ef78a3bc637b0ce1378204d48acdd7875a450488
Artem-Git-rgb <140831498+Artem-Git-rgb@users.noreply.github.com>
2025-04-25 22:29:00 +0300

    Открывал с телефона. Подсветка кнопок

--- notes/settings.py
+++ notes/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-8i*ysev&zo4o^=b+7n@rb-g@@_nv^_nd*%gc=gdhsls*&kzs8y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
